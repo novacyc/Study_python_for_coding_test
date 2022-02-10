2cc06c46ac87bcb378a2729584044a04b43bad43
novacyc <novvacyc@naver.com>
2022-02-10 23:29:48 +0900

    2022.02.10

--- 11_greedy/무지의 먹방 라이브.py
+++ 11_greedy/무지의 먹방 라이브.py
@@ -1,19 +1,25 @@
-https://programmers.co.kr/learn/courses/30/lessons/42891
+#https://programmers.co.kr/learn/courses/30/lessons/42891
+import heapq
 def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
     
-    allkill = sum(food_times)
-    result = 0
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i+1))
     
-    while allkill:
-        for i,v in enumerate(food_times):
-            if k == 0:
-                return (i+1)
-            if v != 0:
-                food_times[i] -= 1
-                allkill -= 1
-                k -= 1
-            else:
-                continue
-    return -1
+    eat_time = 0
+    previous_time = 0
+    length = len(food_times)
+    
+    while eat_time + ((q[0][0] - previous_time) * length) <= k:
+        now = heapq.heappop(q)[0]
+        eat_time += (now -previous_time) * length
+        length -= 1
+        previous_time = now
+    
+    result = sorted(q, key = lambda x:x[1])
+    return result[(k-eat_time)%length][1]
 
+   
 print(solution([3,1,2], 5))
