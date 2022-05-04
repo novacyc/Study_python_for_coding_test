ac8254e44292ef193e8baa233e88daf7074cb1e0
novacyc <novvacyc@naver.com>
2022-05-04 16:55:03 +0900

    .

---
+++ 12_Implementation/문자열 압축.py
@@ -0,0 +1,9 @@
+#https://programmers.co.kr/learn/courses/30/lessons/60057?language=python3
+def solution(s):
+    answer = len(s)
+    div = 2
+    while div < answer:
+        div_result = ''
+        for v in s[::div]:
+            
+    return answer
